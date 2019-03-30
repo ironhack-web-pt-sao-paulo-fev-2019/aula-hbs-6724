2ad9ea2f417ac13684871d48186282fe286880be
Paul <paul.divet5@gmail.com>
2019-03-30 17:29:38 -0300

    Registered partials, added first partial with Kobe, cleaned hbs files of useless code (existent in layout.hbs

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    let data = {
+        layout: false
+    }
+    res.render('index');
+});
+
+app.get('/players', (req, res, next) => {
+    let data = {
+        layout: false
+    }
+    res.render('players');
+});
+
+app.get('/teams', (req, res, next) => {
+    let data = {
+        layout: false
+    }
+    res.render('teams', data);
+});
+
+app.listen(3000);
