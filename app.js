b0075ef5d0af8b36b70605fe4d04c370a44ffabd
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-30 17:30:55 -0300

    adicionando partial para rederizar o componet

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ const hbs     = require('hbs');
 const app     = express();
 const path    = require('path')
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/players', (req, res, next) => {
-  res.render('players');
+    
+  res.render('players',);
 });
 
 app.get('/teams', (req, res, next) => {
-  res.render('teams');
+    let data = {
+        layout: false
+      }
+  res.render('teams',data);
 });
 
 app.listen(3000);
